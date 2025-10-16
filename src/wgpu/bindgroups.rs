17b189ad275f86ed9510a4e2cfb5515b7d0e4f2b
Shawn Xie <shawn0326@163.com>
2025-10-16 18:21:02 +0800

    refactor: clean up

--- src/wgpu/bindgroups.rs
+++ src/wgpu/bindgroups.rs
@@ -1,6 +1,7 @@
 use super::textures::Textures;
 use crate::{
     graphics::{Material, MaterialId},
+    // render::RenderItem,
     scene::Camera,
 };
 use std::{
@@ -183,13 +184,9 @@ pub(super) struct GpuBindGroup {
     pub layout: wgpu::BindGroupLayout,
 }
 
-// 此属性标注数据的内存布局兼容 C-ABI，令其可用于着色器
 #[repr(C)]
-// derive 属性自动导入的这些 trait，令其可被存入缓冲区
 #[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
 struct CameraUniform {
-    // glam 的数据类型不能直接用于 bytemuck
-    // 需要先将 Matrix4 矩阵转为一个 4x4 的浮点数数组
     view_proj: [[f32; 4]; 4],
 }
 
@@ -204,3 +201,21 @@ impl CameraUniform {
         self.view_proj = camera.build_view_projection_matrix().to_cols_array_2d();
     }
 }
+
+// #[repr(C)]
+// #[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+// struct ModelUniform {
+//     model_matrix: [[f32; 4]; 4],
+// }
+
+// impl ModelUniform {
+//     fn new() -> Self {
+//         Self {
+//             model_matrix: glam::Mat4::IDENTITY.to_cols_array_2d(),
+//         }
+//     }
+
+//     fn update_model_matrix(&mut self, render_item: &RenderItem) {
+//         self.model_matrix = render_item.world_matrix.to_cols_array_2d();
+//     }
+// }
